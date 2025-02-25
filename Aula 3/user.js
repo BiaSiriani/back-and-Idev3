3000f5663becdbc1f37942c2f865f99094269da5
BiaSiriani <biasiriani200722@gmail.com>
2025-02-25 10:34:06 -0300

    atalizaçao no code

--- Aula 3/user.js
+++ Aula 3/user.js
@@ -3,7 +3,10 @@ class User {
         this.id = id; // id do usuario
         this.nome = nome; // nome do usuario
         this.email = email; //email usuario
+        this.telefone = telefone;
+        this.endereco = endereco;
     }
+
 }
 
 module.exports = User; //exporta o modulocomat
